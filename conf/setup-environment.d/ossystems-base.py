9dc3d77ec0011ad98bfaf5b7cff05176a619080b
Otavio Salvador <otavio@ossystems.com.br>
2013-06-21 23:54:57 -0300

    setup-environment.d/ossystems-base.py: Set default DISTRO do 'oel'
    Change-Id: I3ed6bc4c2b052bdb4e5b0297ad61ccb3f9be13a8
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- conf/setup-environment.d/ossystems-base.py
+++ conf/setup-environment.d/ossystems-base.py
@@ -1,7 +1,12 @@
+def __set_defaults():
+    set_default('DISTRO', 'oel')
+
 def __after_init_ossystems_base():
     PLATFORM_ROOT_DIR = os.environ['PLATFORM_ROOT_DIR']
+
     append_layers([ os.path.join(PLATFORM_ROOT_DIR, 'sources', p) for p in
                     ['meta-ossystems-base']
                     ])
 
+run_set_defaults(__set_defaults)
 run_after_init(__after_init_ossystems_base)
